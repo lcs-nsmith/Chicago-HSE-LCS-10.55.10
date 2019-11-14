a7c53b34f9bdd50c64427b41243b605066a7d594
Russell Gordon <github@russellgordon.ca>
2019-11-14 16:08:27 -0500

    Initial Commit

---
+++ Chicago-HSE-LCS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chicago-HSE-LCS
+//
+//  Created by Gordon, Russell on 2019-11-14.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
