8c779bcf44c7694d5a65892e3cf01b607fdea36c
Jeewoo Lee <jlee@lcs.on.ca>
2019-11-14 17:00:23 -0500

    got crew struct done

---
+++ Chicago-HSE-LCS/Crew.swift
@@ -0,0 +1,15 @@
+//
+//  Crew.swift
+//  Chicago-HSE-LCS
+//
+//  Created by Lee, Jeewoo on 2019-11-14.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+struct Crew {
+    var name: String
+    var job: String
+    var bio: String
+}
