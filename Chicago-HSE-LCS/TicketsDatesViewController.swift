62df8f782cd6c1772a2861dddba3621cb619bcc7
Selena Yang <syang@lcs.on.ca>
2019-11-17 18:20:20 -0500

    Added empty view and view controller for tickets and dates

---
+++ Chicago-HSE-LCS/TicketsDatesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TicketsDatesViewController.swift
+//  Chicago-HSE-LCS
+//
+//  Created by Yang, Selena on 2019/11/17.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import UIKit
+
+class TicketsDatesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
