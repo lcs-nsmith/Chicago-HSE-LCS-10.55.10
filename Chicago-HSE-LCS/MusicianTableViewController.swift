d4a4e3533ce2c0ef098e793e358c178f63ef9b45
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-17 16:12:27 -0500

    Made musicianViewController background black

--- Chicago-HSE-LCS/MusicianTableViewController.swift
+++ Chicago-HSE-LCS/MusicianTableViewController.swift
@@ -28,15 +28,36 @@ class MusicianTableViewController: UITableViewController {
     
     ]
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
+      // Set the status bar text to be white
+      override var preferredStatusBarStyle: UIStatusBarStyle {
+          return .lightContent
+      }
+      
+      override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          // Uncomment the following line to preserve selection between presentations
+          // self.clearsSelectionOnViewWillAppear = false
+          
+          // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+          // self.navigationItem.rightBarButtonItem = self.editButtonItem
+          
+          // Show the navigation item
+          self.navigationController?.setNavigationBarHidden(false, animated: false)
+          
+          // Get table view to always have black background
+          tableView.backgroundColor = .black
+          
+          // Set the seperator color to always be dark grey
+          tableView.separatorColor = .darkGray
+          
+          // Set a zero-height footer so that no blank cells show after the list of menu items
+          tableView.tableFooterView = UIView()
+          
+          // Signal need to update the status bar
+          self.setNeedsStatusBarAppearanceUpdate()
+          
+      }
 
     // MARK: - Table view data source
 
