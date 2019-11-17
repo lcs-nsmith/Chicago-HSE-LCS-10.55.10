ad6fc1c515e85f08d861c1b75ac6dc39385fef74
Russell Gordon <github@russellgordon.ca>
2019-11-17 18:20:15 -0500

    Added empty and view controller for Art Display

---
+++ Chicago-HSE-LCS/ArtDisplayViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArtDisplayViewController.swift
+//  Chicago-HSE-LCS
+//
+//  Created by Gordon, Russell on 2019-11-17.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import UIKit
+
+class ArtDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
