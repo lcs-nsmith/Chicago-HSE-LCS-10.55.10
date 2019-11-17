f9335eb85cf693c4774490077aeb0ca8c92ae1f5
Liam Davidson <ldavidson@lcs.on.ca>
2019-11-17 15:41:12 -0500

    Added Musician structure file.

---
+++ Chicago-HSE-LCS/MusicianStructure.swift
@@ -0,0 +1,17 @@
+//
+//  MusicianStructure.swift
+//  Chicago-HSE-LCS
+//
+//  Created by Davidson, Liam on 2019-11-17.
+//  Copyright © 2019 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+struct Musician {
+    
+    var name: String
+    var instrument: String
+    var Bio: String
+    var ImageID: String
+}
