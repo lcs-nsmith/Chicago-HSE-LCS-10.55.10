4920fcd1f7e77cc3016c4147b9a45d690c232801
Russell Gordon <rgordon@lcs.on.ca>
2021-02-12 08:55:20 -0500

    Added SwiftUI-based target

---
+++ SwiftUI-Chicago-HSE-LCS/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SwiftUI-Chicago-HSE-LCS
+//
+//  Created by Russell Gordon on 2021-02-11.
+//  Copyright © 2021 Gordon, Russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Chicago-HSE-LCS/SwiftUI_Chicago_HSE_LCSApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_Chicago_HSE_LCSApp.swift
+//  SwiftUI-Chicago-HSE-LCS
+//
+//  Created by Russell Gordon on 2021-02-11.
+//  Copyright © 2021 Gordon, Russell. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chicago_HSE_LCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
