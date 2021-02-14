6f9b543f8758a70a0144e125d49a07de376d79d5
Russell Gordon <rgordon@lcs.on.ca>
2021-02-14 16:08:26 -0500

    Fixed position of Crew option on landing page.

--- SwiftUI-Chicago-HSE-LCS/Views/LandingView.swift
+++ SwiftUI-Chicago-HSE-LCS/Views/LandingView.swift
@@ -21,13 +21,13 @@ struct LandingView: View {
                 Group {
                     Section(header: Text("The Show")) {
                         
-                        NavigationLink(destination: CrewListView()) {
-                            Text("Crew")
-                        }
+                        Text("1")
                         Text("2")
                         Text("3")
                         Text("4")
-                        Text("5")
+                        NavigationLink(destination: CrewListView()) {
+                            Text("Crew")
+                        }
                         Text("6")
                         Text("7")
                         Text("8")
