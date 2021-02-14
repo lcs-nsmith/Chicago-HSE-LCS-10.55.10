8c389b3ce47c394e5ed3ffa77cc15ba53058b36a
Russell Gordon <rgordon@lcs.on.ca>
2021-02-13 19:14:37 -0500

    Got basic grouped list working on main page.

--- SwiftUI-Chicago-HSE-LCS/Views/Landing.swift
+++ SwiftUI-Chicago-HSE-LCS/Views/Landing.swift
@@ -10,13 +10,40 @@ import SwiftUI
 
 struct Landing: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Image("banner")
+                .resizable()
+                .scaledToFit()
+            
+            List {
+                Section(header: Text("The Show")) {
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    Text("4")
+                }
+                Section(header: Text("Intermission")) {
+                    Text("5")
+                    Text("6")
+                    Text("7")
+                }
+                Section(header: Text("Other")) {
+                    Text("8")
+                    Text("9")
+                    Text("10")
+                }
+            }
+            .listStyle(GroupedListStyle())
+            
+            Spacer()
+        }
     }
 }
 
 struct Landing_Previews: PreviewProvider {
     static var previews: some View {
         Landing()
+            .preferredColorScheme(.dark)
     }
 }
