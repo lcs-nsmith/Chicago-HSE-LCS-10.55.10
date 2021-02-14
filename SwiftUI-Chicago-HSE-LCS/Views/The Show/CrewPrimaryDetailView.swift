30d1739a3057f6a2f9c441385a599d20a9ff21c6
Russell Gordon <rgordon@lcs.on.ca>
2021-02-14 13:03:08 -0500

    Added detail view for primary crew members.

---
+++ SwiftUI-Chicago-HSE-LCS/Views/The Show/CrewPrimaryDetailView.swift
@@ -0,0 +1,54 @@
+//
+//  CrewPrimaryDetailView.swift
+//  SwiftUI-Chicago-HSE-LCS
+//
+//  Created by Russell Gordon on 2021-02-14.
+//  Copyright © 2021 Gordon, Russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct CrewPrimaryDetailView: View {
+    
+    var crewMember: Crew
+    
+    var body: some View {
+        
+        ScrollView {
+            
+            VStack(alignment: .leading, spacing: 10) {
+                
+                Image(crewMember.imageId)
+                .resizable()
+                .scaledToFit()
+                
+                Text("Job")
+                    .font(.title2)
+                
+                Text(crewMember.job)
+                
+                if !crewMember.bio.isEmpty {
+                    
+                    Text("Bio")
+                        .font(.title2)
+                    
+                    Text(crewMember.bio)
+                }
+
+            }
+        
+        }
+        .navigationTitle(crewMember.name)
+        .navigationBarTitleDisplayMode(.inline)
+        
+    }
+}
+
+struct CrewPrimaryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CrewPrimaryDetailView(crewMember: primaryCrewMembers.first!)
+                .preferredColorScheme(.dark)
+        }
+    }
+}
