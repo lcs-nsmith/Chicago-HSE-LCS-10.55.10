d0b4e55b4f4bb972009acec7cb21bf93dccc52f9
Russell Gordon <rgordon@lcs.on.ca>
2021-02-14 20:15:21 -0500

    Added more documentation to CrewListView

--- SwiftUI-Chicago-HSE-LCS/Views/The Show/CrewListView.swift
+++ SwiftUI-Chicago-HSE-LCS/Views/The Show/CrewListView.swift
@@ -1,5 +1,5 @@
 //
-//  Crew.swift
+//  CrewListView.swift
 //  SwiftUI-Chicago-HSE-LCS
 //
 //  Created by Russell Gordon on 2021-02-13.
@@ -16,17 +16,21 @@ struct CrewListView: View {
         
         VStack {
             
+            // Include a search bar to filter the lists below
             SearchBarView(text: $searchText)
                 .padding(.top, 20)
             
+            // Show a list with two sections
             List {
                 
                 Group {
                     
+                    // Primary Crew
                     Section(header: Text("Primary Crew")) {
                         
-                        
+                        // Iterate over all defined crew members
                         ForEach(primaryCrewMembers.filter({ crewMember in
+                                                                // When a search is active, filter the list of crew
                                                                 if !searchText.isEmpty {
                                                                     // Only return true when this crew member's name contains the search text
                                                                     return crewMember.name.contains(searchText)
@@ -36,14 +40,20 @@ struct CrewListView: View {
                                                                 }
                                                             })
                         ) { someCrewMember in
+                            
+                            // Make a navigation link for each crew member in the list
                             NavigationLink(destination: CrewPrimaryDetailView(crewMember: someCrewMember)) {
                                 Text(someCrewMember.name)
                             }
                         }
                     }
-                    
+
+                    // Special Teams
                     Section(header: Text("Special Teams")) {
+
+                        // Iterate over all defined special teams
                         ForEach(specialTeams.filter({ team in
+                                                        // When a search is active, filter the list of special teams
                                                         if !searchText.isEmpty {
                                                             // Only return true when this special team's bio contains the search text
                                                             return team.bio.contains(searchText)
@@ -53,6 +63,8 @@ struct CrewListView: View {
                                                         }
                                                     })
                         ) { team in
+                            
+                            // Make a navigation link for each special team in the list
                             NavigationLink(destination: CrewSpecialTeamsDetailView(crewMember: team)) {
                                 Text(team.name)
                             }
