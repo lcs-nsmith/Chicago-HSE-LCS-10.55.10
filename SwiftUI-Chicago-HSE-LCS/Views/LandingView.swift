78e02909268447fc337c2d95ca2a5e1b8c99165b
Nathan Smith <nsmith@lcs.on.ca>
2021-11-20 11:18:30 -0500

    Icons and links done for the 'show' group, I want to change the size of the icons and the links are commented out

--- SwiftUI-Chicago-HSE-LCS/Views/LandingView.swift
+++ SwiftUI-Chicago-HSE-LCS/Views/LandingView.swift
@@ -21,48 +21,116 @@ struct LandingView: View {
                 Group {
                     Section(header: Text("The Show")) {
                         
-                        Text("1")
-                        Text("2")
-                        Text("3")
-                        Text("4")
+                        //                        NavigationLink(destination: TicketsAndDatesView()) {
+                        HStack{
+                            Image(systemName: "ticket")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40)
+                            Text("Tickets and Dates")
+                        }
+                        //                        }
+                        //                        NavigationLink(destination: MusicalNumbersView()) {
+                        HStack{
+                            Image(systemName: "music.note.list")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40)
+                            Text("Musical Numbers")
+                        }
+                        //                        }
+                        
+                        //                        NavigationLink(destination: CharactersListView()) {
+                        HStack{
+                            Image(systemName: "person.crop.artframe")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40)
+                            Text("Characters")
+                        }
+                        //
+                        
+                        //                        NavigationLink(destination: TicketsAndDatesView()) {
+                        HStack{
+                            Image(systemName: "person.3.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 40)
+                            Text("Cast")
+                        }
+                        //
+                        
                         NavigationLink(destination: CrewListView()) {
-                            Text("Crew")
+                            HStack{
+                                Image(systemName: "person.3.sequence")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40)
+                                Text("Crew")
+                            }
                         }
-                        Text("6")
-                        Text("7")
-                        Text("8")
-                    }
-                    Section(header: Text("Intermission")) {
-                        NavigationLink(destination: TheatreLobbyArtDisplayView()) {
-                            Text("Theatre Lobby Art Display")
+                        
+                            //                        NavigationLink(destination: MusiciansListView()) {
+                            HStack{
+                                Image(systemName: "guitars")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40)
+                                Text("Musicians")
+                            }
+                            //
+                            
+                            //                        NavigationLink(destination: FacultyListView()) {
+                            HStack{
+                                Image(systemName: "person.text.rectangle")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40)
+                                Text("Faculty")
+                            }
+                            //
+                            
+                            //                        NavigationLink(destination: ProductionTeamNotesView()) {
+                            HStack{
+                                Image(systemName: "list.and.film")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 40)
+                                Text("Production Team Notes")
+                            }
+                            //
                         }
-                        Text("10")
-                        Text("11")
-                    }
-                    Section(header: Text("Other")) {
-                        NavigationLink(destination: OurMusicalTheatreProgramView()) {
-                            Text("Our Musical Theatre Program")
+                        Section(header: Text("Intermission")) {
+                            NavigationLink(destination: TheatreLobbyArtDisplayView()) {
+                                Text("Theatre Lobby Art Display")
+                            }
+                            Text("10")
+                            Text("11")
                         }
-                        Text("13")
-                        Text("14")
+                        Section(header: Text("Other")) {
+                            NavigationLink(destination: OurMusicalTheatreProgramView()) {
+                                Text("Our Musical Theatre Program")
+                            }
+                            Text("13")
+                            Text("14")
+                        }
                     }
+                    .listRowBackground(Color.black)
                 }
-                .listRowBackground(Color.black)
+                .listStyle(GroupedListStyle())
+                
+                Spacer()
             }
-            .listStyle(GroupedListStyle())
-            
-            Spacer()
-        }
-        // Hide the navigation bar on this page to avoid blank space above banner
-        .navigationBarHidden(true)
-    }
-}
-
-struct LandingView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            LandingView()
-                .preferredColorScheme(.dark)
+            // Hide the navigation bar on this page to avoid blank space above banner
+            .navigationBarHidden(true)
         }
     }
-}
+    
+    struct LandingView_Previews: PreviewProvider {
+        static var previews: some View {
+            NavigationView {
+                LandingView()
+                    .preferredColorScheme(.dark)
+            }
+        }
+    }
